ed9fed65c74335751cd1a211f1be9f2943704b2f
a00761325 <a00761325@SE12L0308-06.ad.bcit.ca>
2016-01-07 15:14:49 -0800

    commit

---
+++ Game.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Game
+ *
+ * @author a00761325
+ */
+class Game {
+    //put your code here
+    var $position;
+    
+    
+    
+    function __construct($squares) {
+$this>
+position = str_split($squares);
+}
+    
+    
+    
+    
+}
+
+
